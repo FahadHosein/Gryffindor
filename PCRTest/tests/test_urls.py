d081b1c12230c88e213492237b5d38d02b4642e4
ninjamankyle <90986358+ninjamankyle@users.noreply.github.com>
2021-11-16 11:11:58 -0400

    Updated test_urls.py file
    Imported functions from django.urls and imported url path from views.py file.

--- PCRTest/tests/test_urls.py
+++ PCRTest/tests/test_urls.py
@@ -1,6 +1,11 @@
 from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from login.views import sitehome #,redirlogin, redirlogout, info, accounterror, faq, advisories 
+
 
 class TestUrls(SimpleTestCase):
 
    def test_sitehome_url_is_resolved(self):
-       assert 1 == 2 
+      url = reverse('sitehome')
+       print(resolve(url))
+       
